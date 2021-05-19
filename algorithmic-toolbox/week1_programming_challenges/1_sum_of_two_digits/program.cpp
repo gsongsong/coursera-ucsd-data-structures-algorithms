f7fafc79d5cfd7fceb4031b7015202f1472c0c1e
Seokseong Jeon <sjeon87@gmail.com>
2021-05-19 13:13:02 +0900

    Add week 1 programming assignment 1: sum of two digits

---
+++ algorithmic-toolbox/week1_programming_challenges/1_sum_of_two_digits/program.cpp
@@ -0,0 +1,17 @@
+/**
+ * In this very first programming challenge, your goal is to implement a
+ * program that reads two digits from the standard input and prints their sum
+ * to the standard output.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	return 0;
+}
+
